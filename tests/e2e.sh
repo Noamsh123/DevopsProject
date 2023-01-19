8c12a6e88d87c4cf1493a8e68db73866fc8b88bf
noam <noam.shamir01@gmail.com>
2023-01-19 14:08:26 +0000

    update

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
-curl 3.8.158.98:80/person
-curl 3.8.158.98:80/person/1
-curl -X POST -d 'firstName=asdf&lastName=asdfg' 3.8.158.98:80/person/1234
-curl -X PUT -d 'age=25' 3.8.158.98:80/person/1234
-curl -X DELETE 3.8.158.98:80/person/1234
+declare -a RESPONSES
+# API=( "/person/1234" "/person/1" "" )
+RESPONSES+=$(curl -s -o /dev/null -w '%{http_code}' --connect-timeout 2 18.130.32.203:80/person)
+RESPONSES+=$(curl -s -o /dev/null -w '%{http_code}' --connect-timeout 2 18.130.32.203:80/person/1)
+RESPONSES+=$(curl -X POST -s -o /dev/null -w '%{http_code}' -d 'firstName=asdf&lastName=asdfg' --connect-timeout 2 18.130.32.203:80/person/1234)
+RESPONSES+=$(curl -X PUT -s -o /dev/null -w '%{http_code}' -d 'age=25' --connect-timeout 2 18.130.32.203:80/person/1234)
+RESPONSES+=$(curl -X DELETE -s -o /dev/null -w '%{http_code}' --connect-timeout 2 18.131.32.203:80/person/1234)
+
+
+# curl -s -o /dev/null -w '%{http_code}' --connect-timeout 3 18.130.32.203:80/person
+# curl -s -o /dev/null -w '%{http_code}' --connect-timeout 3 18.130.32.203:80/person/1
+# curl -X POST -s -o /dev/null -w '%{http_code}' -d 'firstName=asdf&lastName=asdfg' --connect-timeout 3 18.130.32.203:80/person/1234
+# curl -X PUT -s -o /dev/null -w '%{http_code}' -d 'age=25' --connect-timeout 3 18.130.32.203:80/person/1234
+# curl -X DELETE -s -o /dev/null -w '%{http_code}' --connect-timeout 3 18.130.32.203:80/person/1234
+
+
+check=200
+
+for item in "${RESPONSES[@]}"; do
+    echo "check"
+    if [[ $item == *"200"* ]]; then
+        echo "tests faild"
+        exit 1
+    fi
+done
+echo "succese"
+exit 0
