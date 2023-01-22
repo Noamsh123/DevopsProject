7c9e625623f74261614f077f4da7651acfced263
noam <noam.shamir01@gmail.com>
2023-01-22 10:49:33 +0000

    fix volumes problem

---
+++ mysql/setup.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS `players`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `containers-registered`
+--
+
+USE players;
+
+
+CREATE TABLE IF NOT EXISTS `active_players` (
+  `player_id` varchar(15) NOT NULL,
+  `first_name` varchar(10) DEFAULT NULL,
+  `last_name` varchar(10) DEFAULT NULL,
+  `current_team` varchar(10) DEFAULT NULL,
+  `age` int(12) DEFAULT NULL,
+  `country` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`player_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10001 ;
