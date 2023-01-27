997fa187acb16ab387ed598534b725916398ee53
noam <noam.shamir01@gmail.com>
2023-01-27 10:45:11 +0000

    update deploy ip

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-ssh -tt ubuntu@13.40.154.109 << EOF
+ssh -tt ubuntu@35.176.97.86 << EOF
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 644435390668.dkr.ecr.eu-west-2.amazonaws.com
 docker pull 644435390668.dkr.ecr.eu-west-2.amazonaws.com/shamir-repo:1.1.$1
 # replace image of app service to the new image
